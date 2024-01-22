80f110b9dcbcf3960ce5d48a0a2c3e176bb05f85
nelson <peternelson063@gmail.com>
2024-01-22 16:25:17 +0100

    Updated code

--- pipeline_lambda.py
+++ pipeline_lambda.py
@@ -1,20 +1,30 @@
 import boto3
 
 def lambda_handler(event, context):
-    # Enter the names of the pipelines
-    pipeline_names = ['Pipeline1', 'Pipeline2', 'Pipeline3', 'Pipeline4', 'Pipeline5']
+    # define the tag key and value to identify pipelines
+    tag_key = 'Env'
+    tag_value = 'prod'
 
     codepipeline = boto3.client('codepipeline')
 
     try:
-        for pipeline_name in pipeline_names:
+        response = codepipeline.list_pipelines()
+        pipelines = response['pipelines']
+
+        for pipeline in pipelines:
+            pipeline_name = pipeline['name']
+
             try:
-                response = codepipeline.start_pipeline_execution(name=pipeline_name)
-                print(f"Pipeline execution started for {pipeline_name}: {response}")
-            except codepipeline.exceptions.PipelineNotFoundException as ex:
-                print(f"Pipeline {pipeline_name} not found. Skipping...")
-            except Exception as ex:
-                print(f"Error starting pipeline {pipeline_name}: {ex}")
+                response_tags = codepipeline.list_tags_for_resource(resourceArn=f'arn:aws:codepipeline:us-east-1:507698722484:{pipeline_name}')
+
+                for tag in response_tags['tags']:
+                    if tag['key'] == tag_key and tag['value'] == tag_value:
+                        response_execution = codepipeline.start_pipeline_execution(name=pipeline_name)
+                        print(f"Pipeline execution started for {pipeline_name}: {response_execution}")
+            except codepipeline.exceptions.PipelineNotFoundException:
+                print(f"Pipeline '{pipeline_name}' not found. Skipping.")
+            except Exception as e:
+                print(f"Error processing pipeline '{pipeline_name}': {e}")
 
         return {
             'statusCode': 200,
@@ -27,3 +37,4 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': f"Error starting pipeline executions: {e}"
         }
+
